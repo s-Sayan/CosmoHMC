0f7713cf222ce4d74c13769b85f6c6a43ac197f7
data_vadar <rocks.sayan03@gmail.com>
2024-04-08 20:49:39 +0530

    update

--- cosmohmc/almncl/sampler.py
+++ cosmohmc/almncl/sampler.py
@@ -3,7 +3,9 @@ import cosmohmc.distnd.sampler as sampler
 import cosmohmc.almncl.utils as utils
 import healpy as hp
 import numpy as np
-import inspect
+from tqdm import tqdm
+
+#import inspect
 
 class almnclsampler():
     def __init__(self, data, elmax, nside, Cl, Nl, step_size = 0.1, n_steps = 10, n_samples = 1000):
@@ -56,9 +58,181 @@ class almnclsampler():
         self.m_cl.fill(1.0)
         self.m_cl[2:] = utils.mass_cl(self.el[2:], self.Cl[2:], self.Nl[2:])
         
-        self.m_almr, self.m_almi = utils.mass_alm(self.Cl, self.Nl, self.elmax)
+        self.m_almr, self.m_almi = utils.mass_alm(self.Cl, self.Nl, self.n_alms)
         
         
-        #def HMCsample():
+        
+    def leapfrog(self, p_cl, p_almr, p_almi, q_cl, q_almr, q_almi):
+        """
+        The leapfrog integrator for the Hamiltonian Monte Carlo sampler.
+
+        Args:
+            p_cl (array): The momentum for the C_l values.
+            p_almr (array): The momentum for the real part of the alms.
+            p_almi (array): The momentum for the imaginary part of the alms.
+            q_cl (array): The position for the C_l values.
+            q_almr (array): The position for the real part of the alms.
+            q_almi (array): The position for the imaginary part of the alms.
+
+        Returns:
+            p_cl (array): The updated momentum for the C_l values.
+            p_almr (array): The updated momentum for the real part of the alms.
+            p_almi (array): The updated momentum for the imaginary part of the alms.
+            q_cl (array): The updated position for the C_l values.
+            q_almr (array): The updated position for the real part of the alms.
+            q_almi (array): The updated position for the imaginary part of the alms.
+        """
+        
+        el = np.arange(self.elmax+1)
+        for _ in range(self.n_steps):
+            # half momnetum step
+            Cl_hat = utils.Cl_of_almri(q_almr, q_almi, self.elmax)
+            p_cl_dot = utils.pdot_cl(el, Cl_hat, q_cl)
+            p_almr_dot, p_almi_dot = utils.pdot_alm(q_almr, q_almi, self.d_almr, self.d_almi, q_cl, self.Nl, self.n_alms)
+            p_cl = p_cl + 0.5*self.step_size*p_cl_dot
+            p_almr = p_almr + 0.5*self.step_size*p_almr_dot
+            p_almi = p_almi + 0.5*self.step_size*p_almi_dot
             
+            # full position step
+            q_cl = q_cl + self.step_size * p_cl/self.m_cl
+            q_almr = q_almr + self.step_size*p_almr/self.m_almr
+            q_almi = q_almi + self.step_size*p_almi/self.m_almi
+            
+            # half momentum step
+            Cl_hat = utils.Cl_of_almri(q_almr, q_almi, self.elmax)
+            p_cl_dot = utils.pdot_cl(el, Cl_hat, q_cl)
+            p_almr_dot, p_almi_dot = utils.pdot_alm(q_almr, q_almi, self.d_almr, self.d_almi, q_cl, self.Nl, self.n_alms)
+            p_cl = p_cl + 0.5*self.step_size*p_cl_dot
+            p_almr = p_almr + 0.5*self.step_size*p_almr_dot
+            p_almi = p_almi + 0.5*self.step_size*p_almi_dot
+            
+        return p_cl, p_almr, p_almi, q_cl, q_almr, q_almi
+    
+    def leapfrog_v2(self, p_cl, p_almr, p_almi, q_cl, q_almr, q_almi):
+        """
+        The leapfrog integrator for the Hamiltonian Monte Carlo sampler.
+
+        Args:
+            p_cl (array): The momentum for the C_l values.
+            p_almr (array): The momentum for the real part of the alms.
+            p_almi (array): The momentum for the imaginary part of the alms.
+            q_cl (array): The position for the C_l values.
+            q_almr (array): The position for the real part of the alms.
+            q_almi (array): The position for the imaginary part of the alms.
+
+        Returns:
+            p_cl (array): The updated momentum for the C_l values.
+            p_almr (array): The updated momentum for the real part of the alms.
+            p_almi (array): The updated momentum for the imaginary part of the alms.
+            q_cl (array): The updated position for the C_l values.
+            q_almr (array): The updated position for the real part of the alms.
+            q_almi (array): The updated position for the imaginary part of the alms.
+        """
+        
+        el = np.arange(self.elmax+1)
+        for _ in range(self.n_steps):
+            
+            # half position step
+            q_cl_dot = p_cl/self.m_cl
+            q_almr_dot = p_almr/self.m_almr
+            q_almi_dot = p_almi/self.m_almi
+            q_cl = q_cl + 0.5 * self.step_size * q_cl_dot
+            q_almr = q_almr + 0.5 * self.step_size * q_almr_dot
+            q_almi = q_almi + 0.5 * self.step_size * q_almi_dot
+            
+            # full momentum step
+            Cl_hat = utils.Cl_of_almri(q_almr, q_almi, self.elmax)
+            p_cl_dot = utils.pdot_cl(el, Cl_hat, q_cl)
+            p_almr_dot, p_almi_dot = utils.pdot_alm(q_almr, q_almi, self.d_almr, self.d_almi, q_cl, self.Nl, self.n_alms)
+            p_cl = p_cl + self.step_size*p_cl_dot
+            p_almr = p_almr + self.step_size*p_almr_dot
+            p_almi = p_almi + self.step_size*p_almi_dot
+            
+            # half position step
+            q_cl_dot = p_cl/self.m_cl
+            q_almr_dot = p_almr/self.m_almr
+            q_almi_dot = p_almi/self.m_almi
+            q_cl = q_cl + 0.5 * self.step_size * q_cl_dot
+            q_almr = q_almr + 0.5 * self.step_size * q_almr_dot
+            q_almi = q_almi + 0.5 * self.step_size * q_almi_dot
+            
+        return p_cl, p_almr, p_almi, q_cl, q_almr, q_almi
+            
+            
+    def HMCsample(self):
+        """Generate samples using the Hamiltonian Monte Carlo algorithm."""
+        
+        #Initialise the loop
+        q_cl = np.copy(self.data_cl)
+        q_almr = np.copy(self.d_almr)
+        q_almi = np.copy(self.d_almi)
+
+        #just as a placeholder value
+        q_cl_star = np.copy(q_cl)
+        q_almr_star = np.copy(q_almr)
+        q_almi_star = np.copy(q_almi)
+        
+        Cl_hat = np.copy(q_cl)
+        
+        sample_q_almr = []
+        sample_q_almi = []
+        sample_q_cl = []
+        dH_array = []
+        KE_term0_array = []
+        PE_term0_array = []
+        KE_term_star_array = []
+        PE_term_star_array = []
+        
+        accepted = 0
+        
+        for _ in tqdm(range(self.n_samples), desc="Sampling (HMC)"):
+            
+            p_almr, p_almi = utils.momentum_alm(self.m_almr, self.m_almi, self.n_alms)
+            p_cl = utils.momentum_cl(self.m_cl, self.elmax)
+            
+            KE_term0 = utils.KE(p_cl, p_almr, p_almi, self.m_cl, self.m_almr, self.m_almi, self.n_alms)
+            
+            Dl_hat = utils.Cl_of_almri(self.d_almr - q_almr, self.d_almi-q_almi, self.elmax)
+            Cl_hat = utils.Cl_of_almri(q_almr, q_almi, self.elmax)
+            PE_term0 = utils.PE(Cl_hat, Dl_hat, q_cl, self.Nl)
+            
+            H0 = KE_term0 + PE_term0
+            
+            #Leapfrog integration
+            #p_cl_star, p_almr_star, p_almi_star, q_cl_star, q_almr_star, q_almi_star = self.leapfrog(p_cl, p_almr, p_almi, q_cl, q_almr, q_almi)            
+            p_cl_star, p_almr_star, p_almi_star, q_cl_star, q_almr_star, q_almi_star = self.leapfrog_v2(p_cl, p_almr, p_almi, q_cl, q_almr, q_almi)
+            
+            KE_term_star = utils.KE(p_cl_star, p_almr_star, p_almi_star, self.m_cl, self.m_almr, self.m_almi, self.n_alms)
+            Dl_hat = utils.Cl_of_almri(self.d_almr - q_almr_star, self.d_almi - q_almi_star, self.elmax)
+            Cl_hat = utils.Cl_of_almri(q_almr_star, q_almi_star, self.elmax)
+            PE_term_star = utils.PE(Cl_hat, Dl_hat, q_cl_star, self.Nl)
+            
+            H_star = KE_term_star + PE_term_star
+            dH = H_star - H0
+            accpt_prob = np.exp(-dH)
+            
+            # Accpect or reject
+            if np.random.rand() < accpt_prob:
+                q_cl = q_cl_star
+                q_almr = q_almr_star
+                q_almi = q_almi_star
+                accepted += 1
+                
+            sample_q_cl.append(q_cl)
+            sample_q_almr.append(q_almr)
+            sample_q_almi.append(q_almi)
+            dH_array.append(dH)
+            KE_term0_array.append(KE_term0)
+            PE_term0_array.append(PE_term0)
+            KE_term_star_array.append(KE_term_star)
+            PE_term_star_array.append(PE_term_star)
+            
+        self.acceptance_rate = accepted / self.n_samples
+        self.dH_array = dH_array
+        self.KE_term0_array = KE_term0_array
+        self.PE_term0_array = PE_term0_array
+        self.KE_term_star_array = KE_term_star_array
+        self.PE_term_star_array = PE_term_star_array
+        return np.array(sample_q_cl), np.array(sample_q_almr), np.array(sample_q_almi)
+                
         
--- cosmohmc/almncl/utils.py
+++ cosmohmc/almncl/utils.py
@@ -1,128 +1,223 @@
+# File: cosmohmc/almncl/utils.py
+
 import numpy as np
 import healpy as hp
 
-def n2lm_index(n):
-    """Input: one dimensional index of alm. Output: corresponding (l,m) index"""
+def n2lm_index(n: int) -> tuple[int, int]:
+    """
+    Converts a one-dimensional index to a two-dimensional (l, m) index.
+
+    Args:
+    - n (int): The one-dimensional index of alm.
+
+    Returns:
+    - tuple[int, int]: The corresponding (l, m) index.
+    """
     lt = (np.sqrt(8.0*n+1)-1.0)/2.0
     l = int(lt)
-    m = int(n-l*(l+1)/2)
-    return l,m
+    m = int(n - l*(l+1)/2)
+    return l, m
 
-def lm2n_index(l,m):
-    """Input: (l,m) index of the alm. Output: corresponding one dimensional index."""
-    n = int(l*(l+1)/2 + m)
-    return n
+def lm2n_index(l: int, m: int) -> int:
+    """
+    Converts a two-dimensional (l, m) index to a one-dimensional index.
 
-def mass_cl(ell, Cel = 1.0, Nel = 1.0):
-    """Input: el & Cl, for l = 2 to elmax. Output: the mass for Cl parameters."""
+    Args:
+    - l (int): The spherical harmonic degree.
+    - m (int): The order of the spherical harmonic degree.
+
+    Returns:
+    - int: The corresponding one-dimensional index.
+    """
+    return int(l*(l+1)/2 + m)
+
+def mass_cl(ell: int, Cel: float = 1.0, Nel: float = 1.0) -> float:
+    """
+    Calculates the mass for Cl parameters.
+
+    Args:
+    - ell (int): The spherical harmonic degree.
+    - Cel (float, optional): The power spectrum Cl. Defaults to 1.0.
+    - Nel (float, optional): The noise Nel. Defaults to 1.0.
+
+    Returns:
+    - float: The mass for Cl parameters.
+    """
     return 0.5*(2*ell+1)/(Cel+Nel)**2
 
-def n_alms(elmax):
-    """Input: elmax. Output: number of alms for elmax."""
+def n_alms(elmax: int) -> int:
+    """
+    Calculates the number of alms for a given elmax.
+
+    Args:
+    - elmax (int): The maximum spherical harmonic degree.
+
+    Returns:
+    - int: The number of alms up to and including elmax.
+    """
     return int((elmax+1)*(elmax+2)/2)
 
-def momentum_cl(m_cl, elmax):
-    """Input: the mass of Cl, elmax. Output: the (elmax-1) length array of momentum for Cl."""
-    return np.sqrt(m_cl)*np.random.normal(0.0, 1.0, elmax+1)
+def momentum_cl(m_cl: np.ndarray, elmax: int) -> np.ndarray:
+    """
+    Generates a random momentum array for Cl parameters.
 
-def mass_alm(Cl, Nl, n_alms):
-    """Input: Cl, elmax. Output: mass of real and imaginary part of alm"""
-    """For the time being, assume data Cl is exact Cl"""
-    m_almr = np.ndarray(shape = (n_alms))
-    m_almi = np.ndarray(shape = (n_alms))
-    m_almr.fill(1.0)
-    m_almi.fill(1.0)
-    #excluding alms for l = 0, and l = 1
-    for i in np.arange(3, n_alms, 1):
-        l,m = n2lm_index(i)
+    Args:
+    - m_cl (np.ndarray): The mass of Cl parameters.
+    - elmax (int): The maximum spherical harmonic degree.
+
+    Returns:
+    - np.ndarray: An array of momentum values for Cl.
+    """
+    return np.sqrt(m_cl) * np.random.normal(0.0, 1.0, elmax+1)
+
+def mass_alm(Cl: np.ndarray, Nl: np.ndarray, n_alms: int) -> tuple[np.ndarray, np.ndarray]:
+    """
+    Calculates the mass of the real and imaginary parts of alm.
+
+    Args:
+    - Cl (np.ndarray): The power spectrum Cl.
+    - Nl (np.ndarray): The noise Nl.
+    - n_alms (int): The number of alms.
+
+    Returns:
+    - tuple[np.ndarray, np.ndarray]: Arrays representing the mass of the real and imaginary parts of alm.
+    """
+    m_almr = np.ones(n_alms)
+    m_almi = np.ones(n_alms)
+    # Exclude alms for l = 0 and l = 1
+    for i in range(3, n_alms):
+        l, m = n2lm_index(i)
         if m == 0:
-            #posterior variance of alm, when m = 0
-            m_almr[i] = 1.0/Cl[l] + 1.0/Nl[l]
-            m_almi[i] = 1.0
+            m_almr[i] = 1.0 / Cl[l] + 1.0 / Nl[l]
         else:
-            #posterior variance of alm, when m != 0
-            m_almr[i] = 2.0/Cl[l] + 2.0/Nl[l]
-            m_almi[i] = 2.0/Cl[l] + 2.0/Nl[l]
+            m_almr[i] = m_almi[i] = 2.0 / Cl[l] + 2.0 / Nl[l]
     return m_almr, m_almi
 
-def momentum_alm(m_almr, m_almi, n_alms):
-    """Input: n_alms, m_almr, m_almi, 
-    Output: momenum for real and imaginary part of alm"""
-    p_almr = np.sqrt(m_almr)*np.random.normal(0, 1.0, n_alms)
-    p_almi = np.sqrt(m_almi)*np.random.normal(0, 1.0, n_alms)
+def momentum_alm(m_almr: np.ndarray, m_almi: np.ndarray, n_alms: int) -> tuple[np.ndarray, np.ndarray]:
+    """
+    Generates random momentum arrays for the real and imaginary parts of alm.
+
+    Args:
+    - m_almr (np.ndarray): The mass array for the real part of alm.
+    - m_almi (np.ndarray): The mass array for the imaginary part of alm.
+    - n_alms (int): The number of alms.
+
+    Returns:
+    - tuple[np.ndarray, np.ndarray]: Momentum arrays for the real and imaginary parts of alm.
+    """
+    p_almr = np.sqrt(m_almr) * np.random.normal(0, 1.0, n_alms)
+    p_almi = np.sqrt(m_almi) * np.random.normal(0, 1.0, n_alms)
     return p_almr, p_almi
 
-def pdot_alm(q_almr, q_almi, d_almr, d_almi, q_cl, Nl, n_alms):
-    """Input:q_almr, q_almi, d_almr, d_almi, q_cl, Nl, n_alms. 
-    Output: momentum derivative for real and imaginary part of alms- p_almr_dot & p_almi_dot."""
-    p_almr_d = np.ndarray(shape = (n_alms))
-    p_almi_d = np.ndarray(shape = (n_alms))
-    p_almr_d.fill(0.0)
-    p_almi_d.fill(0.0)
-    for k in np.arange(3, n_alms, 1):
-        l,m = n2lm_index(k)
+def pdot_alm(q_almr: np.ndarray, q_almi: np.ndarray, d_almr: np.ndarray, d_almi: np.ndarray, q_cl: np.ndarray, Nl: np.ndarray, n_alms: int) -> tuple[np.ndarray, np.ndarray]:
+    """
+    Calculates the momentum derivative for the real and imaginary parts of alms.
+
+    Args:
+    - q_almr (np.ndarray): The real part of alms.
+    - q_almi (np.ndarray): The imaginary part of alms.
+    - d_almr (np.ndarray): The real part of data alms.
+    - d_almi (np.ndarray): The imaginary part of data alms.
+    - q_cl (np.ndarray): The power spectrum derived from alms.
+    - Nl (np.ndarray): The noise Nl.
+    - n_alms (int): The number of alms.
+
+    Returns:
+    - tuple[np.ndarray, np.ndarray]: Momentum derivative arrays for the real and imaginary parts of alms.
+    """
+    p_almr_d = np.zeros(n_alms)
+    p_almi_d = np.zeros(n_alms)
+    for k in range(3, n_alms):
+        l, m = n2lm_index(k)
         if m == 0:
-            p_almr_d[k] = -1.0*q_almr[k]/q_cl[l] + (d_almr[k] - q_almr[k])/Nl[l]
-            p_almi_d[k] = 0.0
+            p_almr_d[k] = -q_almr[k] / q_cl[l] + (d_almr[k] - q_almr[k]) / Nl[l]
         else:
-            p_almr_d[k] = -2.0*q_almr[k]/q_cl[l] + 2.0*(d_almr[k] - q_almr[k])/Nl[l]
-            p_almi_d[k] = -2.0*q_almi[k]/q_cl[l] + 2.0*(d_almi[k] - q_almi[k])/Nl[l]
+            p_almr_d[k] = p_almi_d[k] = -2 * q_almr[k] / q_cl[l] + 2 * (d_almr[k] - q_almr[k]) / Nl[l]
     return p_almr_d, p_almi_d
 
-def pdot_cl(el, Cl_hat, Cl):
-    """Input: Cl_hat (Cl obtained using alms) and the Cl parameter. 
-    Output: momentum derivative for Cl."""
-    elmax = len(Cl)-1
-    p_cl_d = np.ndarray(shape = (elmax+1))
-    p_cl_d[0] = 0.0
-    p_cl_d[1] = 0.0
-    p_cl_d[2:] = 0.5*(2*el[2:]+1.0)*(Cl_hat[2:]/Cl[2:] - 1.0)/Cl[2:]
+def pdot_cl(el: np.ndarray, Cl_hat: np.ndarray, Cl: np.ndarray) -> np.ndarray:
+    """
+    Calculates the momentum derivative for Cl.
+
+    Args:
+    - el (np.ndarray): The spherical harmonic degrees.
+    - Cl_hat (np.ndarray): The Cl obtained using alms.
+    - Cl (np.ndarray): The power spectrum Cl parameters.
+
+    Returns:
+    - np.ndarray: The momentum derivative for Cl.
+    """
+    elmax = len(Cl) - 1
+    p_cl_d = np.zeros(elmax + 1)
+    p_cl_d[2:] = 0.5 * (2 * el[2:] + 1.0) * (Cl_hat[2:] / Cl[2:] - 1.0) / Cl[2:]
     return p_cl_d
 
 #define the Hamiltonian
-def KE(p_cl, p_almr, p_almi, m_cl, m_almr, m_almi, n_alms):
-    """Computes the kinetic energy term of the Hamiltonian.
-    Input: p_cl, p_almr, p_almi, m_cl, m_almr, m_almi, n_alms.
-    Output: Kinetic energy."""
-    term1 = 0.5*np.sum(p_cl[2:]**2/m_cl[2:])
-    term2 = 0.5*np.sum(p_almr[3:]**2/m_almr[3:])
+def KE(p_cl: np.ndarray, p_almr: np.ndarray, p_almi: np.ndarray, m_cl: np.ndarray, m_almr: np.ndarray, m_almi: np.ndarray, n_alms: int) -> float:
+    """
+    Computes the kinetic energy term of the Hamiltonian.
+
+    Args:
+    - p_cl (np.ndarray): Momentum for Cl.
+    - p_almr (np.ndarray): Momentum for the real part of alm.
+    - p_almi (np.ndarray): Momentum for the imaginary part of alm.
+    - m_cl (np.ndarray): Mass for Cl.
+    - m_almr (np.ndarray): Mass for the real part of alm.
+    - m_almi (np.ndarray): Mass for the imaginary part of alm.
+    - n_alms (int): The number of alms.
+
+    Returns:
+    - float: The kinetic energy.
+    """
+    term1 = 0.5 * np.sum(p_cl[2:]**2 / m_cl[2:])
+    term2 = 0.5 * np.sum(p_almr[3:]**2 / m_almr[3:])
+    #term3 = 0.5 * sum(p_almi[k]**2 / m_almi[k] for k in range(3, n_alms) if n2lm_index(k)[1] != 0)
     term3 = 0.0
     for k in np.arange(3, n_alms, 1):
         l,m = n2lm_index(k)
         if m != 0:
             term3 += p_almi[k]**2/m_almi[k]
     term3 = 0.5*term3
-    KE = term1 + term2 + term3
-    return KE
+    return term1 + term2 + term3
 
-#define potential energy
-def PE(Cl_hat, Dl_hat, Cl, Nl):
-    """Computes the potential energy term of the Hamiltonian.
-    Input: Cl_hat (Cl obtained using alms), 
-           Dl_hat (Power spectrum of difference between data alms and sample alms)
-           Cl, Nl.
-    Output: Potential energy."""
+def PE(Cl_hat: np.ndarray, Dl_hat: np.ndarray, Cl: np.ndarray, Nl: np.ndarray) -> float:
+    """
+    Computes the potential energy term of the Hamiltonian.
+
+    Args:
+    - Cl_hat (np.ndarray): The Cl obtained using alms.
+    - Dl_hat (np.ndarray): The power spectrum of the difference between data alms and sample alms.
+    - Cl (np.ndarray): The power spectrum Cl parameters.
+    - Nl (np.ndarray): The noise Nl.
+
+    Returns:
+    - float: The potential energy.
+    """
     el = np.arange(len(Cl))
-    term1 = 0.5*np.sum((2.0*el[2:]+1.0)*Dl_hat[2:]/Nl[2:])
-    term2 = 0.5*np.sum((2.0*el[2:]+1.0)*Cl_hat[2:]/Cl[2:])
-    term3 = 0.5*np.sum((2.0*el[2:]+1.0)*np.log(Cl[2:]))
-    PE = term1 + term2 + term3
-    return PE
+    term1 = 0.5 * np.sum((2.0 * el[2:] + 1.0) * Dl_hat[2:] / Nl[2:])
+    term2 = 0.5 * np.sum((2.0 * el[2:] + 1.0) * Cl_hat[2:] / Cl[2:])
+    term3 = 0.5 * np.sum((2.0 * el[2:] + 1.0) * np.log(Cl[2:]))
+    return term1 + term2 + term3
 
-#compute Cl_hat
-def Cl_of_almri(almr, almi, elmax):
-    """Computes the Cl for the given alms (real and imaginary part separately)"""
-    Cl_h = np.ndarray(shape = (elmax+1))
-    Cl_h[0] = 0.0
-    Cl_h[1] = 0.0
-    for l in np.arange(2, elmax+1, 1):
-        temp = 0.0
-        k = lm2n_index(l,0)
-        temp = almr[k]**2
-        for m in np.arange(1, l + 1, 1):
-            k = lm2n_index(l,m)
-            temp += 2.0*(almr[k]**2 + almi[k]**2)
-        Cl_h[l] = temp/(2.0*l + 1.0)
+def Cl_of_almri(almr: np.ndarray, almi: np.ndarray, elmax: int) -> np.ndarray:
+    """
+    Computes the Cl for the given alms (real and imaginary part separately).
+
+    Args:
+    - almr (np.ndarray): The real part of alms.
+    - almi (np.ndarray): The imaginary part of alms.
+    - elmax (int): The maximum spherical harmonic degree.
+
+    Returns:
+    - np.ndarray: The computed Cl.
+    """
+    Cl_h = np.zeros(elmax + 1)
+    for l in range(2, elmax + 1):
+        temp = almr[lm2n_index(l, 0)]**2
+        for m in range(1, l + 1):
+            k = lm2n_index(l, m)
+            temp += 2.0 * (almr[k]**2 + almi[k]**2)
+        Cl_h[l] = temp / (2.0 * l + 1.0)
     return Cl_h
 
 
